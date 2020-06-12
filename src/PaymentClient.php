f6e767ab0c6870df35f213bbf13dfa0ba464ab83
ashishtyagi9622 <ashish.tyagi@tripock.com>
2020-06-12 16:39:58 +0530

    changes for receiver_id payer_id and order_id

--- src/PaymentClient.php
+++ src/PaymentClient.php
@@ -12,6 +12,7 @@ class PaymentClient
 
     private $base_url;
     private $app_key;
+    const AKSHAMAALA_USER_ID = 'AKSHAMAALA:u132n231nti';
 
     public function __construct($base_url="http://payment/api",$key="uwtqeijugsajdgw564e6e5tfhsaluwqiwqha"){
         $this->base_url = $base_url;
@@ -137,6 +138,7 @@ class PaymentClient
                 $client = new Client();
                 $url = $this->base_url.'/wallets/retailers/'.$data['receiver_id'].'/balance';
                 $client->setDefaultOption('headers', [ 'Content-Type' => 'application/json','app-key'=>$this->app_key ]);
+                $data['payer_id']=self::AKSHAMAALA_USER_ID;
                 $content = [
                     'json' => $data
                 ];
@@ -206,6 +208,7 @@ class PaymentClient
                 $client = new Client();
                 $url = $this->base_url.'/wallets/suppliers/'.$data['receiver_id'].'/balance';
                 $client->setDefaultOption('headers', [ 'Content-Type' => 'application/json','app-key'=>$this->app_key ]);
+                $data['payer_id']=self::AKSHAMAALA_USER_ID;
                 $content = [
                     'json' => $data
                 ];
@@ -319,6 +322,76 @@ class PaymentClient
     public function doRequest(array $data=[]){
         try{
 
+            $validation = $this->validatePaymentRequestData($data);
+
+            if (!$validation['status']) {
+                return $validation;
+            } else {
+
+                $payment_breakup = $data['payment_breakup'];
+
+                $total_amount_pay = 0;
+
+                foreach ($payment_breakup as $payment_data){
+                    $payment_by = $payment_data['type'];
+                    $amount = $payment_data['amount'];
+                    $total_amount_pay = $total_amount_pay + $amount;
+                }
+
+                // if($total_amount_pay != $data['total_amount']){
+                //     return ['status'=>false,'message'=>"Total amount is not sum up with payment breakup"];
+                // }
+
+                $client = new Client();
+                $url = $this->base_url.'/payment';
+                $client->setDefaultOption('headers', [ 'Content-Type' => 'application/json','app-key'=>$this->app_key ]);
+                $content = [
+                    'json' => $data
+                ];
+                $res = $client->post($url, $content);
+                $response_data = json_decode($res->getBody()->getContents(),true);
+                $payload = $response_data['payload'];
+                $payload_data = $payload['data'];
+                $message = $payload['message'];
+                $messageStatus = '';
+                $statusResult='';
+                foreach( $payload_data['transaction_status'] as $type ) {
+                    $statusResult=strtoupper($type['status']);
+                    if ( $type['status'] == 'failed' || $type['status'] == 'pending') {
+
+                        $messageStatus = $type['payment_method'].' payment '.$type['status'];
+                        $statusResult=strtoupper($type['status']);
+                        break;
+
+                    }
+                }
+                $transaction_status=['resultStatus'=>$statusResult,'message'=>$messageStatus,'status_response'=>$payload_data['transaction_status']];
+                return ['status'=>$response_data['status'],'message'=>$message,'transaction_id'=>implode(':',$payload_data['transaction_id']),'transaction_type'=>implode(':',$payload_data['transaction_type']),'transaction_status'=>$transaction_status];
+            }
+
+        }catch (ClientException $ex){
+            if($ex->hasResponse()){
+                $response_data = json_decode($ex->getResponse()->getBody()->getContents(),true);
+                $payload = $response_data['payload'];
+                return ['status'=>false,"message"=>$payload['message'],'code'=>$response_data['code']];
+            }
+            return ['status'=>false,'message'=>"some payment service connection error with no error response"];
+        }catch (RequestException $ex){
+            if($ex->hasResponse()){
+                $response_data = json_decode($ex->getResponse()->getBody()->getContents(),true);
+                $payload = $response_data['payload'];
+                return ['status'=>false,"message"=>$payload['message'],'code'=>$response_data['code']];
+            }
+            return ['status'=>false,'message'=>"some payment service connection error with no error response"];
+        }
+        catch (\Exception $ex){
+            return ["status"=>false,"message"=>"some client error, contact to developer",'ex'=>$ex];
+        }
+    }
+
+    public function doRequestRetailer(array $data=[]){
+        try{
+            $data['receiver_id'] = self::AKSHAMAALA_USER_ID;
             $validation = $this->validatePaymentRequestData($data);
 
             if (!$validation['status']) {
@@ -806,7 +879,6 @@ class PaymentClient
             $url = $this->base_url.'/wallets/retailers';
             $client->setDefaultOption('headers', [ 'Content-Type' => 'application/json','app-key'=>$this->app_key ]);
             $head = [];
-            $body = $data;
             $content = [
                 'json' => ($data)
             ];
@@ -844,7 +916,6 @@ class PaymentClient
             $url = $this->base_url.'/wallets/suppliers';
             $client->setDefaultOption('headers', [ 'Content-Type' => 'application/json','app-key'=>$this->app_key ]);
             $head = [];
-            $body = $data;
             $content = [
                 'json' => ($data)
             ];
