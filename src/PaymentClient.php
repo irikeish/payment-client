c98f4086496191bc8f032a252664a0bb2def1ee5
ashishtyagi9622 <ashish.tyagi@tripock.com>
2020-07-27 21:19:32 +0530

    verify otp for chotta credit

--- src/PaymentClient.php
+++ src/PaymentClient.php
@@ -998,10 +998,10 @@ class PaymentClient
         }
     }
 
-    public function verifyOtp(array $data = []) {
+    public function verifyOtpInputWallet(array $data = []) {
         try{
             $client = new Client();
-            $url = $this->base_url.'/verifyOtp';
+            $url = $this->base_url.'/input-wallet/verify-otp';
             $client->setDefaultOption('headers', [ 'Content-Type' => 'application/json','app-key'=>$this->app_key ]);
             $head = [];
             $body = $data;
@@ -1049,10 +1049,61 @@ class PaymentClient
         }
     }
 
-    public function verifyOtpOrder(array $data = []) {
+    public function verifyOtpChottaCredit(array $data = []) {
         try{
             $client = new Client();
-            $url = $this->base_url.'/verifyOtp';
+            $url = $this->base_url.'/chotta-credit/verify-otp';
+            $client->setDefaultOption('headers', [ 'Content-Type' => 'application/json','app-key'=>$this->app_key ]);
+            $head = [];
+            $body = $data;
+            $content = [
+                'json' => ($data)
+            ];
+            $res = $client->post($url, $content);
+            $response_str = $res->getBody()->getContents();
+            $response_data = json_decode($response_str,true);
+            $payload = $response_data['payload'];
+            $payload_data = $payload['data'];
+            $message = $response_data['payload']['message'];
+
+            $messageStatus = '';
+            $statusResult='';
+            foreach( $payload_data['transaction_status'] as $type ) {
+                $statusResult=strtoupper($type['status']);
+                if ( $type['status'] == 'failed' || $type['status'] == 'pending') {
+
+                    $messageStatus = $type['payment_method'].' payment '.$type['status'];
+                    $statusResult=strtoupper($type['status']);
+                    break;
+
+                }
+            }
+            $transaction_status=['resultStatus'=>$statusResult,'message'=>$messageStatus,'status_response'=>$payload_data['transaction_status']];
+            return ['status'=>$response_data['status'],'message'=>$message,'transaction_id'=>implode(':',$payload_data['transaction_id']),'transaction_type'=>implode(':',$payload_data['transaction_type']),'transaction_status'=>$transaction_status,'wallet_balance'=>$payload_data['wallet_balance']];        
+        }catch (ClientException $ex){
+            if($ex->hasResponse()){
+                $response_data = json_decode($ex->getResponse()->getBody()->getContents(),true);
+                $payload = $response_data['payload'];
+                return ['status'=>false,"message"=>$payload['message']];
+            }
+            return ['status'=>false,'message'=>"some payment client connection error with no error response"];
+        }catch (RequestException $ex){
+            if($ex->hasResponse()){
+                $response_data = json_decode($ex->getResponse()->getBody()->getContents(),true);
+                $payload = $response_data['payload'];
+                return ['status'=>false,"message"=>$payload['message']];
+            }
+            return ['status'=>false,'message'=>"some payment service connection error with no error response"];
+        }
+        catch (\Exception $ex){
+            return ["status"=>false,"message"=>"some client error, contact to developer",'ex'=>$ex];
+        }
+    }
+
+    public function resendOtpInputWallet(array $data = []) {
+        try{
+            $client = new Client();
+            $url = $this->base_url.'/input-wallet/resend-otp';
             $client->setDefaultOption('headers', [ 'Content-Type' => 'application/json','app-key'=>$this->app_key ]);
             $head = [];
             $body = $data;
@@ -1100,10 +1151,10 @@ class PaymentClient
         }
     }
 
-    public function resendOtp(array $data = []) {
+    public function resendOtpChottaCredit(array $data = []) {
         try{
             $client = new Client();
-            $url = $this->base_url.'/resendOtp';
+            $url = $this->base_url.'/chotta-credit/resend-otp';
             $client->setDefaultOption('headers', [ 'Content-Type' => 'application/json','app-key'=>$this->app_key ]);
             $head = [];
             $body = $data;
